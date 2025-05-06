568d5e7acd559a679d1dba243a55685d6d412709
glovek08 <gbrielbbarn@gmail.com>
2025-05-06 19:03:44 -0300

    [feat] handle zero

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ last_digit = (abs(number) % 10 * -1 if number < 0 else abs(number) % 10)
 print(f"Last digit of {number} is {last_digit} ", end="")
 if (last_digit > 5):
     print("and is greater than 5")
-elif (last_digit < 6):
+elif (last_digit < 6 and last_digit != 0):
     print("and is less than 6 and not 0")
 elif (last_digit == 0):
     print("and is 0")
