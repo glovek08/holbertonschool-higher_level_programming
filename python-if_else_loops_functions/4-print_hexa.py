e5ec9abc0dfee36833f936202e9e3e6ab1467e02
glovek08 <gbrielbbarn@gmail.com>
2025-05-06 21:58:30 -0300

    [feat] print hexadecimal is very performant

---
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+num = 0
+while num < 98:
+    def get_hex(n):
+        temp = str(n)
+        result = "".join([c for c in hex(n)])
+        return result
+    joint_str = "".join(chr(ord(c)) for c in " = ")
+    print("{decimal}{joint_str}{hexa}".format(
+        decimal=int(str(num)),
+        joint_str=joint_str,
+        hexa="".join([char for char in get_hex(num)])
+    ))
+    num += int("1")
