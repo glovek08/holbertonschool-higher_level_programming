82102e6e83c59501d730b56591da508ded0edc27
glovek08 <gbrielbbarn@gmail.com>
2025-05-07 15:55:13 -0300

    [pycodestyle] mostly compliant

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
+
 for num in range(100):
-  print("{num:02}".format(num = num), end=", " if num < 99 else "\n")
+    print("{num:02}".format(num=num), end=", " if num < 99 else "\n")
