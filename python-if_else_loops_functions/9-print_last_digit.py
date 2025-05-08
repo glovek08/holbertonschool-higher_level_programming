ad668800ea880192ec02b69026a62a4795f2c34d
glovek08 <gbrielbbarn@gmail.com>
2025-05-08 20:49:48 -0300

    [fix] handle negative

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
 def print_last_digit(num):
-    res = num % 10
+    if (num < 0):
+        res = ((num * -1) % 10) * -1
+    else:
+        res = num % 10
     print(res, end="")
     return (res)
+
+
+print_last_digit(958)
