7c879d60e39be083e1a5c11b0f3653704aab3d32
glovek08 <gbrielbbarn@gmail.com>
2025-05-08 16:08:55 -0300

    [fix] keep punctuations and symbols

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,9 +3,11 @@ def uppercase(str):
     str_to_byte = []
     for char in str:
         char = ord(char)
-        if (char >= 65 and char <= 122):
+        if (char >= 32 and char <= 126):
             if (char >= 97 and char <= 122):
                 char -= 32
             str_to_byte.append(chr(char))
     print("{str}".format(str = (''.join(str_to_byte))))
+
+uppercase("!@#!@#   !@#!@#123sfsfasDWDAs23 2 ")
 
