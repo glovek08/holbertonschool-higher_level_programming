11c92e8ff29ec32e66a7aa11cee1ce69e1d0ceb4
glovek08 <gbrielbbarn@gmail.com>
2025-05-08 13:24:55 -0300

    [fix] parse byte

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def islower(num):
-    return (True if (num >= 97 and num <= 122) else False)
+def islower(char):
+    char = ord(char)
+    return (True if (char >= 97 and char <= 122) else False)
