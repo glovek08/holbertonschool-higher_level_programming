d28ea7c82ea1949228535a9e1fedafcc51f9c976
glovek08 <gbrielbbarn@gmail.com>
2025-05-09 18:27:34 -0300

    chore: pycodestyle

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ from calculator_1 import add, sub, mul, div
 b = 5
 a = 10
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, add(a,b)))
-    print("{} - {} = {}".format(a, b, sub(a,b)))
-    print("{} * {} = {}".format(a, b, mul(a,b)))
-    print("{} / {} = {}".format(a, b, div(a,b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
