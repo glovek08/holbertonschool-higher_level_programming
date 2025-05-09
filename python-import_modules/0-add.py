435358c4312f26a41f2565f1ea792f9d351a40f3
glovek08 <gbrielbbarn@gmail.com>
2025-05-09 13:29:09 -0300

    chore: pycodestyle

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,7 +2,6 @@
 from add_0 import add
 
 if __name__ == "__main__":
-    a = 1; b = 2
-    print("{a} + {b} = {add_fun}".format(a=a, b=b, add_fun=add(a,b)))
-
-
+    a = 1
+    b = 2
+    print("{a} + {b} = {add_fun}".format(a=a, b=b, add_fun=add(a, b)))
