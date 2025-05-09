2c2b3dbb6ade4a6c04419778e548c97f226710cf
glovek08 <gbrielbbarn@gmail.com>
2025-05-08 21:04:06 -0300

    feat: fizzbuzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(100):
+        res = ''
+        if i % 3 == 0:
+            res += 'Fizz'
+        if i % 5 == 0:
+            res += 'Buzz'
+        print(res or i)
