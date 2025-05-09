7264b085d819017e5d77a38a6ffa205d8d0ca7d6
glovek08 <gbrielbbarn@gmail.com>
2025-05-09 18:24:08 -0300

    fix: module alias

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import calculator_1 as calc1
+import calculator_1
 b = 5
 a = 10
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, calc1.add(a,b)))
-    print("{} - {} = {}".format(a, b, calc1.sub(a,b)))
-    print("{} * {} = {}".format(a, b, calc1.mul(a,b)))
-    print("{} / {} = {}".format(a, b, calc1.div(a,b)))
+    print("{} + {} = {}".format(a, b, calculator_1.add(a,b)))
+    print("{} - {} = {}".format(a, b, calculator_1.sub(a,b)))
+    print("{} * {} = {}".format(a, b, calculator_1.mul(a,b)))
+    print("{} / {} = {}".format(a, b, calculator_1.div(a,b)))
