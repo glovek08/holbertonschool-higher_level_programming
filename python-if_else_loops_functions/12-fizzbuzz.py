8ba6e8503434623e31fbef1add73fa594875fc83
glovek08 <gbrielbbarn@gmail.com>
2025-05-08 21:09:47 -0300

    fix: 1 to 100 range

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(100):
+    for i in range(1, 100):
         res = ''
         if i % 3 == 0:
             res += 'Fizz'
         if i % 5 == 0:
             res += 'Buzz'
         print((res or i), end=" ")
+fizzbuzz()
