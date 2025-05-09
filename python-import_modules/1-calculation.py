bbcff7fcb7ad9a563dc3871ee40fb0bf105523dc
glovek08 <gbrielbbarn@gmail.com>
2025-05-09 18:22:01 -0300

    fix: derpify function

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-import inspect
 import calculator_1 as calc1
 b = 5
 a = 10
-i = 0
-operator = ['+', '-', '*', '/']
 if __name__ == "__main__":
-    calc_functions = {
-        name: obj for name, obj in inspect.getmembers(calc1, inspect.isfunction)
-    } 
-    for fname in calc_functions:
-        print("{} {} {} = {}".format(a, operator[i], b, calc_functions[fname](a, b)))
-        i += 1
-    i = 0
+    print("{} + {} = {}".format(a, b, calc1.add(a,b)))
+    print("{} - {} = {}".format(a, b, calc1.sub(a,b)))
+    print("{} * {} = {}".format(a, b, calc1.mul(a,b)))
+    print("{} / {} = {}".format(a, b, calc1.div(a,b)))
