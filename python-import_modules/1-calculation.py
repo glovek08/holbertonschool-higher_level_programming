8b995deb73fdc059e4ad27ffeae1712b07d8305f
glovek08 <gbrielbbarn@gmail.com>
2025-05-09 18:06:33 -0300

    fix: proper print format

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,9 +3,18 @@ import inspect
 import calculator_1 as calc1
 b = 5
 a = 10
+i = 0
+operator = ['+', '-', '*', '/']
+    # DERP MODE?
+    # print("{} + {} = {}".format(a, b, calc1.add(a,b)))
+    # print("{} - {} = {}".format(a, b, calc1.sub(a,b)))
+    # print("{} * {} = {}".format(a, b, calc1.mul(a,b)))
+    # print("{} / {} = {}".format(a, b, calc1.div(a,b)))
 if __name__ == "__main__":
     calc_functions = {
         name: obj for name, obj in inspect.getmembers(calc1, inspect.isfunction)
     } 
     for fname in calc_functions:
-        print("{}".format(calc_functions[fname](a, b)))
+        print("{} {} {} = {}".format(a, operator[i], b, calc_functions[fname](a, b)))
+        i += 1
+    i = 0
