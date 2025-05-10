02bd80445bc50167a0726fc0b9e040a7c5aabde0
glovek08 <gbrielbbarn@gmail.com>
2025-05-10 18:22:49 -0300

    fix: run on import

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 from sys import argv
 
-argc = len(argv) - 1
-if (argc == 0):
-    print("0 arguments.")
-    exit()
-elif (argc == 1):
-    print("1 argument:")
-else:
-    print(f"{argc} arguments:")
-    argv.pop(0)
-for arg in argv:
-    print(f"{argv.index(arg) + 1}: {arg}")
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    if (argc == 0):
+        print("0 arguments.")
+        exit()
+    elif (argc == 1):
+        print("1 argument:")
+    else:
+        print(f"{argc} arguments:")
+        argv.pop(0)
+    for arg in argv:
+        print(f"{argv.index(arg) + 1}: {arg}")
