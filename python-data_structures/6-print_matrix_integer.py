88516e5a7688b10b013abc92b607759887e852b5
glovek08 <gbrielbbarn@gmail.com>
2025-05-13 16:44:08 -0300

    fix: fng decimal formatter

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,6 +6,6 @@ def print_matrix_integer(matrix=[[]]):
     for outer in matrix:
         if (i != matrix_len):
             for inner in matrix[i]:
-                print("{el}".format(el = inner), end=' ')
+                print("{el:d}".format(el = inner), end=' ')
         print()
         i += 1
