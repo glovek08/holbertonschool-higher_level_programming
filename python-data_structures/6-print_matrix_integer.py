ebdd94d0ac1e84655a8d1ee481640b4a6d73fe6f
glovek08 <gbrielbbarn@gmail.com>
2025-05-13 16:43:16 -0300

    feat: print list matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    matrix_len = len(matrix)
+    i = 0
+    for outer in matrix:
+        if (i != matrix_len):
+            for inner in matrix[i]:
+                print("{el}".format(el = inner), end=' ')
+        print()
+        i += 1
