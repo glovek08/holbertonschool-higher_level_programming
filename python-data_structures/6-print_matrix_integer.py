e74af9a3c39ecb626357198020b1e390da095b5e
glovek08 <gbrielbbarn@gmail.com>
2025-05-13 16:58:41 -0300

    fix: newline for last index of inner list

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 def print_matrix_integer(matrix=[[]]):
     matrix_len = len(matrix)
     i = 0
     for outer in matrix:
         if (i != matrix_len):
             for inner in matrix[i]:
-                print("{:d}".format(inner), end=' ')
+                if (matrix[i].index(inner) + 1 != len(matrix[i])):
+                    print("{:d}".format(inner), end=' ')
+                else:
+                    print("{:d}".format(inner), end='')
         print()
         i += 1
+print_matrix_integer(matrix)
