59c04733020adad4d0942ba51fbf290d595eadfe
glovek08 <gbrielbbarn@gmail.com>
2025-05-13 20:36:31 -0300

    fix: initializing max_value with first element

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
     if (list_len == 0):
         return None
     
-    max_value = 0
+    max_value = my_list[0]
     for each in my_list:
         if each > max_value:
             max_value = each
