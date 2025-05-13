68ce455b0703353bb72c29631b4b06634a73d0ec
glovek08 <gbrielbbarn@gmail.com>
2025-05-13 18:27:31 -0300

    fix: wrong conditional logic

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,19 +2,22 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     #tuple_a
-    if (len(tuple_a) != 2):
-        a_len = len(tuple_a)
-        if (a_len == 0):
-            tuple_a = (0, 0)
-        elif (a_len < 2):
-            tuple_a = (tuple_a[0], 0)
+    a_len = len(tuple_a)
+    if (a_len == 0):
+        tuple_a = (0, 0)
+    elif (a_len == 1):
+        tuple_a = (tuple_a[0], 0)
+    else:
+        tuple_a = (tuple_a[0], tuple_a[1])
+
     #tuple_b
-    elif (len(tuple_b) != 2):
-        b_len = len(tuple_b)
-        if (b_len == 0):
-            tuple_b = (0, 0)
-        elif (b_len < 2):
-            tuple_b = (tuple_b[0], 0)
-        
-    new_tuple = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+    b_len = len(tuple_b)
+    if (b_len == 0):
+        tuple_b = (0, 0)
+    elif (b_len == 1):
+        tuple_b = (tuple_b[0], 0)
+    else:
+        tuple_b = (tuple_b[0], tuple_b[1])
+
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return new_tuple
