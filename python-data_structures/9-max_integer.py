980f0127606267718fcc7acd8c4fa8eefc93e1c8
glovek08 <gbrielbbarn@gmail.com>
2025-05-13 20:39:26 -0300

    chore: pycodestyle

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,7 +4,6 @@ def max_integer(my_list=[]):
     list_len = len(my_list)
     if (list_len == 0):
         return None
-    
     max_value = my_list[0]
     for each in my_list:
         if each > max_value:
