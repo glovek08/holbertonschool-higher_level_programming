4f9186d0c14f8eec23c91d129b4e4ade909b3ef6
glovek08 <gbrielbbarn@gmail.com>
2025-05-13 16:00:40 -0300

    fix: shallow copy (?)

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-import copy
 
 def new_in_list(my_list, idx, element):
     if (idx < 0) or (idx >= len(my_list)):
         return my_list
     else:
-        new_list = copy.deepcopy(my_list)
+        new_list = my_list[:]
         new_list[idx] = element
         return new_list
