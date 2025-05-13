4c80eb0a71eba71684949e84c61232799266adfa
glovek08 <gbrielbbarn@gmail.com>
2025-05-13 19:55:14 -0300

    fix: debug stuff

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -6,8 +6,7 @@ def max_integer(my_list=[]):
         return None
     
     max_value = 0
-    print(my_list, list_len)
     for each in my_list:
         if each > max_value:
             max_value = each
-    print(max_value)
+    return max_value
