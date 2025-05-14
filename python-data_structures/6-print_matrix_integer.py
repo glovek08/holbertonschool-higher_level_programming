764f6b6318f45002c7f31b48bc40a48f7749a0ba
glovek08 <gbrielbbarn@gmail.com>
2025-05-13 21:39:33 -0300

    refactor: list comprehension instead of nested loops

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    matrix_len = len(matrix)
-    for outer in range(matrix_len):
-        row = matrix[outer]
-        row_length = len(row)
-        for inner in range(row_length):
-            print("{:d}".format(row[inner]), end='')
-            if inner != row_length - 1:
-                print(" ", end='')
-        print()
+    [print(' '.join(["{:d}".format(num) for num in row])) for row in matrix]
