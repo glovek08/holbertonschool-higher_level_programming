8df5e76fe2722f2bc34cf30ee0daae2e32d58216
glovek08 <gbrielbbarn@gmail.com>
2025-05-14 23:32:51 -0300

    chore: pycodestyle

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    diff_list = [x for x in set_1 if x not in set_2] + [y for y in set_2 if y not in set_1]
+    diff_list = [x for x in set_1 if x not in set_2]
+    + [y for y in set_2 if y not in set_1]
     return set(diff_list)
