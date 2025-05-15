d17bc78bc9d745cca2387494b500ed8a87668490
glovek08 <gbrielbbarn@gmail.com>
2025-05-15 19:41:28 -0300

    feat: return best score

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# dict1 = {'a':0, 'b':0, 'c':0, 'd':0, 'e':0}
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    sorted_dict = a_dictionary.copy()
+    try:
+        sorted_dict = (sorted(sorted_dict.items(), key=lambda item: item[1]))
+        return sorted_dict[-1][0]
+    except Exception as error:
+        print(error)
+        return None
+# print(f"Before: {dict1}")
+# print(f"After: {best_score(dict1)}")
