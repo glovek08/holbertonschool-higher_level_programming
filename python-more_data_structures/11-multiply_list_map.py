67d3628d2066549e3eca94fffea5247a94a7f757
glovek08 <gbrielbbarn@gmail.com>
2025-05-16 16:17:57 -0300

    fix: remove comments

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 
-# list1 = [2, 10, 12, 21]
-
 def multiply_list_map(my_list=[], number=0):
     new_list = map(lambda el: el * number, my_list)
     return list(new_list)
-
-# print(multiply_list_map(list1, 2))
-# print(list1)
