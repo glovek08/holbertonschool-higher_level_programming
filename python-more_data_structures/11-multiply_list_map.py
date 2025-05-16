997b269c8ba2cc210248d14c05ad78dc1f2d9a59
glovek08 <gbrielbbarn@gmail.com>
2025-05-16 16:17:18 -0300

    fix: remove error handling

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -3,11 +3,7 @@
 # list1 = [2, 10, 12, 21]
 
 def multiply_list_map(my_list=[], number=0):
-    try:
-        new_list = map(lambda el: el * number, my_list)
-    except Exception as error:
-        print(error)
-        return my_list
+    new_list = map(lambda el: el * number, my_list)
     return list(new_list)
 
 # print(multiply_list_map(list1, 2))
