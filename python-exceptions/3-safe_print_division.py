b0a2329965ac276de5cc5e5c3d8c5a351f651850
glovek08 <gbrielbbarn@gmail.com>
2025-05-17 20:55:19 -0300

    fix: return on finally

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        return None
+        result = None
     finally:
-        print("Inside result: {:d}".format(result))
-        return (result)
+        print("Inside result: {}".format(result))
+        return result
