c55035e2b25b84b68786ca2d4f5f74c2b818dad5
glovek08 <gbrielbbarn@gmail.com>
2025-05-17 17:26:30 -0300

    fix: check if passed arg is valid

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -21,23 +21,27 @@ def get_roman_value(char):
 
 
 def roman_to_int(roman_string):
+    
+    if not (isinstance(roman_string, str)) or (roman_string == None):
+        return 0
+    
     result = 0
     max_int = 3999
 
-    s = roman_string.upper()
-    n = len(s)
+    roman_string = roman_string.upper()
+    roman_str_len = len(roman_string)
 
     i = 0
-    while i < n:
-        current_char = s[i]
+    while i < roman_str_len:
+        current_char = roman_string[i]
         current_value = get_roman_value(current_char)
 
         if current_value == 0:
             i += 1
             continue
 
-        if i + 1 < n:
-            next_char = s[i + 1]
+        if i + 1 < roman_str_len:
+            next_char = roman_string[i + 1]
             next_value = get_roman_value(next_char)
 
             if current_value < next_value:
