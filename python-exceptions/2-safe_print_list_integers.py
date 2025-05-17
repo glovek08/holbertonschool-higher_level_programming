5e355db412dca0824e3fb6ff01a3cf97b2c0863d
glovek08 <gbrielbbarn@gmail.com>
2025-05-17 20:38:18 -0300

    fix: returns on first try

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -10,6 +10,5 @@ def safe_print_list_integers(my_list=[], x=0):
             i += 1
     except (TypeError, ValueError) as error:
         pass
-    finally:
-        print()
-        return (i)
+    print()
+    return (i)
