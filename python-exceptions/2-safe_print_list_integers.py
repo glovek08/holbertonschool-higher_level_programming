d364825150549e8ee888e0f77d2c1b944a65e6e5
glovek08 <gbrielbbarn@gmail.com>
2025-05-17 20:39:42 -0300

    fix: range based approach

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        i = 0
-        for each in my_list:
-            if (i >= x):
-                break
-            print("{:d}".format(each), end=" ")
+    i = 0
+    for j in range(x):
+        try:
+            print("{:d}".format(my_list[j]), end="")
             i += 1
-    except (TypeError, ValueError) as error:
-        pass
+        except (TypeError, ValueError):
+            pass
     print()
-    return (i)
+    return(i)
