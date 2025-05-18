5f543ebf209a4716d140229af3a01ae39286c6e0
glovek08 <gbrielbbarn@gmail.com>
2025-05-18 16:02:04 -0300

    feat: element wise division of matrix with doctest

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+"""
+    This module divides a matrix aosdkosakdkqwed
+    asodaspdkqw
+    asdokapwiokdiopawpoidad Who the actual fuck writes this kind of
+    doc nowadays!!??
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+        Divides the elements of the matrix by a given divisor.
+        Divisor must be either int or float.
+
+        matrix: 2-D list of int or float elements.
+        div: the divisor.
+
+        Raises TypeError if the matrix doesn't contain int or float,
+               or if div is not a number, or if the rows are not of equal size.
+        Raises ZeroDivisionError if div is zero.
+    """
+    if not isinstance(matrix, list) or not all(
+            isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists)\
+            of integers/floats")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    row_length = len(matrix[0])
+    new_matrix = []
+
+    for row in matrix:
+        if len(row) != row_length:
+            raise TypeError("Each row of the matrix must have the same size")
+        new_row = []
+        for column in row:
+            if not isinstance(column, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists)\
+                    of integers/floats")
+            new_row.append(round(column / div, 2))
+        new_matrix.append(new_row)
+
+    return new_matrix
