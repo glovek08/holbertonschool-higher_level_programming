ea216e3a7eb0cc38701f259b783f2c5abcba46a6
glovek08 <gbrielbbarn@gmail.com>
2025-05-18 16:09:59 -0300

    fix: TypeError message for invalid matrix elements

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -24,24 +24,29 @@ def matrix_divided(matrix, div):
             isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists)\
             of integers/floats")
-
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
-    row_length = len(matrix[0])
+    if not matrix:
+        pass
+    row_length = 0
+    if matrix and len(matrix) > 0:
+        row_length = len(matrix[0])
     new_matrix = []
-
     for row in matrix:
-        if len(row) != row_length:
+        if not isinstance(row, list):
+            raise TypeError("matrix must be a matrix\
+                (array of arrays of integers/floats)")
+
+        if len(row) != row_length and matrix:
             raise TypeError("Each row of the matrix must have the same size")
+
         new_row = []
         for column in row:
             if not isinstance(column, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists)\
-                    of integers/floats")
+                raise TypeError("matrix must be a matrix\
+                    (array of arrays of integers/floats)")
             new_row.append(round(column / div, 2))
         new_matrix.append(new_row)
 
