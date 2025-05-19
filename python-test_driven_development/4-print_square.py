bf5f33783506a37419fc8092f5948d880dc0a73a
glovek08 <gbrielbbarn@gmail.com>
2025-05-18 22:04:47 -0300

    fix: blank line after size 0

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -3,13 +3,13 @@
     This module is now well documented.
 """
 def print_square(size: int = 0):
-    """
-        This documentation is excellent.
-    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
+    if size == 0:
+        print()
+        return
     for i in range(size):
         row_string = "#" * size
         print(row_string)
