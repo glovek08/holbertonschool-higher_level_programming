f52687c0bc6e7cac9e4b66e3633b9b3b40be5ae1
glovek08 <gbrielbbarn@gmail.com>
2025-05-20 19:27:41 -0300

    fix: attribute naming

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -9,7 +9,7 @@ class Square:
     """
         This is a square class, now it initalizes
         the private attribute of size
-        (int) __Square_size: Square's size. 
+        (int) __size: Square's size. 
     """
     def __init__(self, size):
-        self.__Square_size = size
+        self.__size = size
