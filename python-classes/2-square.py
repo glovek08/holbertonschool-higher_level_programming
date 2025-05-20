ed09b5b241eb3b497fdde735bf537e19b74c5358
glovek08 <gbrielbbarn@gmail.com>
2025-05-20 20:18:16 -0300

    feat: square now has input handle

---
+++ python-classes/2-square.py
@@ -0,0 +1,20 @@
+#/usr/bin/python3
+"""
+    This module defines a square class.
+"""
+
+
+class Square:
+    """
+        This class defines a square. It receives a size,
+        which is of type int and must not be lower than 0,
+        defaults to 0 if no value is given.
+        
+        (int) __size: Square's size.
+    """
+    def __init__(self, size: int = 0):
+        if isinstance(int, size):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
