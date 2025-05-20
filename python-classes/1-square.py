9a30c23e6fdb4bacfd25b2324c9889e63d94dce6
glovek08 <gbrielbbarn@gmail.com>
2025-05-20 19:23:53 -0300

    feat: square class defitinition now accepts size attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    This module creates a square class with
+    a single private attribute.
+"""
+
+
+class Square:
+    """
+        This is a square class, now it initalizes
+        the private attribute of size
+        (int) _size: Square's size. 
+    """
+    def __init__(self, size):
+        self._size = size
