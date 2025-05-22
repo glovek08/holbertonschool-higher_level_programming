dd7a45389f24f3e40f6bb35882dfcef893e11c9a
glovek08 <gbrielbbarn@gmail.com>
2025-05-21 22:16:09 -0300

    refactor: switch black -> autopep8, custom docstring indent

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -13,23 +13,26 @@ class Node():
         Class Node represents an individual node of a singly
         linked list.
     """
+
     def __init__(self, data: int = 0, next_node: Node = "None"):
         self.data = data
         self.next_node = next_node
-    
+
     @property
     def data(self) -> int:
         return self.__data
+
     @data.setter
     def data(self, value: int = 0):
         if (isinstance(value, int)):
             self.__data = value
         else:
             raise data_type_error
-    
+
     @property
     def next_node(self) -> Node:
         return self.__next_node
+
     @next_node.setter
     def next_node(self, value):
         if value is None or isinstance(value, Node):
@@ -37,13 +40,14 @@ class Node():
         else:
             raise next_node_type_error
 
+
 class SinglyLinkedList():
     """
         Class that creates a singly linked list.
     """
+
     def __init__(self):
         pass
-
 
     def sorted_insert(self, value: Node = None):
         linked_list.append(Node)
--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -2,10 +2,8 @@
 """
     This module defines a Square class.
 """
-
-
 custom_type_error = TypeError("position must be a tuple of 2\
- positive integers")
+positive integers")
 
 
 def sum_tuple(tuple_: tuple) -> int:
@@ -19,52 +17,46 @@ def sum_tuple(tuple_: tuple) -> int:
 class Square:
     """
         Represents a square with a given size.
-
         Attributes:
             __size (int): The size of the square's side. Must be >= 0.
     """
-
     SQUARE_SYMBOL = "#"
 
     def __init__(self, size: int = 0, position: tuple = (0, 0)):
         """
-            Initializes a Square instance.
-
+        Initializes a Square instance.
             Args:
                 size (int): The size of the square. Must be an integer >= 0.
                 position (tuple): A square coordinate. \
                     Must be an int tuple !negative
-
             Raises:
                 TypeError: If size is not int, position not positive \
                     int tuple or length != 2
-                ValueError: If size is less than 0.
+            ValueError: If size is less than 0.
         """
-        if not (isinstance(position, tuple)):
+    if not (isinstance(position, tuple)):
+        raise custom_type_error
+    elif (len(position) != 2):
+        raise custom_type_error
+    for item in position:
+        if not isinstance(item, int):
             raise custom_type_error
-        elif (len(position) != 2):
+        elif (item < 0):
             raise custom_type_error
-        for item in position:
-            if not isinstance(item, int):
-                raise custom_type_error
-            elif (item < 0):
-                raise custom_type_error
-            # Use this code if coordinates are used to replace a specific
-            # character from the square instead of using them for Off-setting
-            # the square.
-            # if (item > size -1):
-            #     position = (0, 0)
-            #     print(f"Warning: Out of bounds position, Square Size:\
-            #       {size}. Position Reset.")
-
-        self.size = size
-        self.__position = position  # uses the setter?
+        # Use this code if coordinates are used to replace a specific
+        # character from the square instead of using them for Off-setting
+        # the square.
+        # if (item > size -1):
+        #  position = (0, 0)
+        #  print(f"Warning: Out of bounds position, Square Size:\
+        #   {size}. Position Reset.")
+    self.size = size
+    self.position = position  # uses the setter
 
     @property
     def size(self) -> int:
         """
             Retrieves the size of the square.
-
             Returns:
                 int: The current size of the square.
         """
@@ -74,17 +66,15 @@ class Square:
     def size(self, value: int):
         """
             Sets the size of the square.
-
             Args:
                 value (int): The new size value.
-
             Raises:
                 TypeError: If value is not an integer.
                 ValueError: If value is less than 0.
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        if (value < 0):
             raise ValueError("size must be >= 0")
         self.__size = value
 
@@ -92,7 +82,6 @@ class Square:
     def position(self) -> int:
         """
             Retrieves a Square's coordinates.
-
             Returns: or not -> ?
                 int: The position at the given coordinate.
         """
@@ -103,15 +92,14 @@ class Square:
         """
             Sets the
         """
-        for item in value:
-            if not (isinstance(item, int)):
-                raise custom_type_error
+    for item in value:
+        if not (isinstance(item, int)):
+            raise custom_type_error
         self.__position = value
 
     def area(self) -> int:
         """
             Calculates the area of the square.
-
             Returns:
                 int: The area of the square.
         """
@@ -122,7 +110,6 @@ class Square:
             Prints the square object.
             If size = 0, it prints nothing.
         """
-
         if (self.size == 0):
             print("")
         else:
@@ -132,8 +119,7 @@ class Square:
                 print(" " * self.position[0], end="")
                 for column in range(self.size):
                     print(Square.SQUARE_SYMBOL, end="")
-                print()
-
+            print()
 # new_square = Square(4, (3, 3))
 # new_square.my_print()
 # print(new_square.position)
