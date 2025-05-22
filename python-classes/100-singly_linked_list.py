617c2de4031afdc82dca4e1baf4ed7c33d3fcbdc
glovek08 <gbrielbbarn@gmail.com>
2025-05-22 14:10:54 -0400

    feat: append node to list

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -1,20 +1,20 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """
     This module contains a Node class to create a
     singly linked list.
 """
 
 data_type_error = TypeError("data must be an integer")
-next_node_type_error = TypeError("next_node must be a Node object")
+node_type_error = TypeError("next_node must be a Node object")
 
 
-class Node():
+class Node:
     """
         Class Node represents an individual node of a singly
         linked list.
     """
 
-    def __init__(self, data: int = 0, next_node: Node = "None"):
+    def __init__(self, data: int = "0", next_node: Node = None):
         self.data = data
         self.next_node = next_node
 
@@ -23,11 +23,10 @@ class Node():
         return self.__data
 
     @data.setter
-    def data(self, value: int = 0):
-        if (isinstance(value, int)):
-            self.__data = value
-        else:
+    def data(self, value):
+        if not isinstance(value, int):
             raise data_type_error
+        self.__data = value
 
     @property
     def next_node(self) -> Node:
@@ -35,19 +34,48 @@ class Node():
 
     @next_node.setter
     def next_node(self, value):
-        if value is None or isinstance(value, Node):
-            self.__next_node = value
-        else:
-            raise next_node_type_error
+        if value is not None and not isinstance(value, Node):
+            raise node_type_error
+        self.__next_node = value
 
 
-class SinglyLinkedList():
+class SinglyLinkedList:
     """
-        Class that creates a singly linked list.
+        Creates a singly linked list.
     """
 
     def __init__(self):
-        pass
+        self.__head = None
 
-    def sorted_insert(self, value: Node = None):
-        linked_list.append(Node)
+    def __str__(self):
+        """
+            Make the list printable - prints each node number on a line.
+        """
+        if self.__head is None:
+            return ""
+
+        result = []
+        current = self.__head
+        while current is not None:
+            result.append(str(current.data))
+            current = current.next_node
+
+        return "\n".join(result)
+
+    def sorted_insert(self, value):
+        """
+            Insert a new Node into the correct sorted\
+                position in the list (increasing order).
+        """
+        new_node = Node(value)
+
+        if self.__head is None or value < self.__head.data:
+            new_node.next_node = self.__head
+            self.__head = new_node
+            return
+
+        current = self.__head
+        while current.next_node is not None and current.next_node.data < value:
+            current = current.next_node
+        new_node.next_node = current.next_node
+        current.next_node = new_node
