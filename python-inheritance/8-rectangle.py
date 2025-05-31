bd601f674c46613dc6c374fd948476cc0ff228d2
glovek08 <gbrielbbarn@gmail.com>
2025-05-31 15:36:21 -0300

    feat: rectangle object inhertis from BaseGeometry

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,26 +1,39 @@
 #!/usr/bin/python3
 """
-Module that defines an BaseGeometry class that validates input but does
-nothing else.
+Module that defines a class for creating geometric objects
 """
 
 
 class BaseGeometry:
     """
-    Empty class
+    Creates geometric objects
     """
     def area(self):
         raise Exception(f"{self.area.__name__}() is not implemented")
 
     def integer_validator(self, name: str, value: int):
+        """
+        Validate that a given alue is a positive integer
+        """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         elif value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-        self.__name = name
-        self.__value = value
+        return True
+
 
 class Rectangle(BaseGeometry):
+    """
+    Creates a rectangle
+    """
     def __init__(self, width: int, height: int):
+        """
+        Initialize rectangle with width and height
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.height * self.width
