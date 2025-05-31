fdac679098abd62520a3a3e7b378228e80835d86
glovek08 <gbrielbbarn@gmail.com>
2025-05-31 17:42:35 -0300

    fix: remove integer only condition

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -14,21 +14,19 @@ def add_attribute(an_obj: object, attr: str, value: int):
     Raises:
         TypeError: if new attribute coulnd't be added.
     """
-    if not isinstance(value, int):
-        raise TypeError("can't add new attribute")
     if not hasattr(an_obj, '__dict__'):
         if not hasattr(an_obj, attr):
             raise TypeError("can't add new attribute")
     setattr(an_obj, attr, value)
 
-class Mamacita:
-    def __init__(self, rand):
-        self.rand = rand
+# class Mamacita:
+#     def __init__(self, rand):
+#         self.rand = rand
 
-uy = Mamacita("Sexy")
-print(uy.rand)
-add_attribute(uy, "edad", 29)
-print(uy.edad)
+# uy = Mamacita("Sexy")
+# print(uy.rand)
+# add_attribute(uy, "edad", 29)
+# print(uy.edad)
 # add_attribute(uy, "is_traba", "yes")
 # print(uy.is_traba)
-print(uy.__dict__)
+# print(uy.__dict__)
