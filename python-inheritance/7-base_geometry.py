210eed692561e60a0e4427a89bd929fcc9d51593
glovek08 <gbrielbbarn@gmail.com>
2025-05-31 14:56:29 -0300

    fix: printing wrong argument in exception message

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -14,7 +14,7 @@ class BaseGeometry:
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         elif value <= 0:
-            raise ValueError(f"{value} must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
         self.name = name
         self.value = value
 
