fa03619c233e4d4ad53ee1522a3c09d356a50670
glovek08 <gbrielbbarn@gmail.com>
2025-05-31 14:45:48 -0300

    feat: validate value and name

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module that defines an empty BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class
+    """
+    def area(self):
+        raise Exception(f"{self.area.__name__}() is not implemented")
+    def integer_validator(self, name, value):
+        if not isinstance(name, int):
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{value} must be greater than 0")
+        self.name = name
+        self.value = value
+
+# base1 = BaseGeometry()
+# base1.area()
