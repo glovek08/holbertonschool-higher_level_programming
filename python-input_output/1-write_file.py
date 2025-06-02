4621d23f562f2a7cbbae19caac1b60ceb4b23950
glovek08 <gbrielbbarn@gmail.com>
2025-06-02 18:27:43 -0300

    fix: return characters written

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -14,8 +14,10 @@ def write_file(filename="", input_text=""):
     with open(filename, mode="w", encoding="utf-8") as a_file:
         try:
             # print(a_file.writable())
-            a_file.write(r"{input_text}")
+            characters_writte = a_file.write(input_text)
         except Exception as error:
             print(error)
+            return 0
+        return characters_writte
 
-# write_file("test123.txt", "Hello mamacita")
+# write_file("test123.txt", "Holberton School is so cool!\n")
