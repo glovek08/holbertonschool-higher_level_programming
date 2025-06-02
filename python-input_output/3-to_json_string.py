6bacb207f292c6dc18af84ba75ff2a9363578956
glovek08 <gbrielbbarn@gmail.com>
2025-06-02 20:13:58 -0300

    feat: docstring

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -6,6 +6,13 @@ import json
 
 
 def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string).
+    Args:
+        my_obj: The object to serialize to JSON.
+    Returns:
+        str: The JSON string representation of the object.
+    """
     try:
         return json.dumps(my_obj)
     except Exception as error:
