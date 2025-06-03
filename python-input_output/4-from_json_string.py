a9e63b52d88b5451a3703925af601cc3e29ab186
glovek08 <gbrielbbarn@gmail.com>
2025-06-02 21:36:49 -0300

    feat: docstring, pycodestyle

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 """
+Module with function to handle JSON shit
 """
 import json
 
+
 def from_json_string(my_str):
+    """
+    Takes a string and produces its object repr (Python data structure)
+    represented by a JSON string.
+    Args:
+        my_str: The JSON string to get its representation in python style.
+    Returns:
+        str: The object repr in python style from a JSON string.
+    """
     try:
         return json.loads(my_str)
     except TypeError:
