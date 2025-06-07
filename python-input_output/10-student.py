fefd1be9459ae889179d6e202bef0a689df9d056
glovek08 <gbrielbbarn@gmail.com>
2025-06-07 14:32:04 -0300

    fix: no import Student class

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -4,10 +4,16 @@ Module that extends the Student class and
 gives it the functionality to filter down
 the JSON string.
 """
-Student = __import__("9-student").Student
 
 
 class Student(Student):
+    def __init__(self, first_name: str = "n/a",
+                last_name: str = "n/a",
+                age: int = 0):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
     def to_json(self, attrs=None):
         if attrs is None:
             return self.__dict__
