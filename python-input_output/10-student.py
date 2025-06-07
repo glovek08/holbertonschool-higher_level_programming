bf82b94d26450dce8236c8d4d29e9bb87a004526
glovek08 <gbrielbbarn@gmail.com>
2025-06-07 14:30:46 -0300

    fix: don't serialize inside method

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -4,19 +4,18 @@ Module that extends the Student class and
 gives it the functionality to filter down
 the JSON string.
 """
-import json
 Student = __import__("9-student").Student
 
 
 class Student(Student):
     def to_json(self, attrs=None):
         if attrs is None:
-            return json.dumps(self.__dict__)
+            return self.__dict__
         my_dict = {}
         for attr in attrs:
             if hasattr(self, attr):
                 my_dict[attr] = getattr(self, attr)
-        return json.dumps(my_dict)
+        return my_dict
 
 # student1 = Student('Jefe', 'Color', 25)
 # print(student1.to_json(["first_name"]))
