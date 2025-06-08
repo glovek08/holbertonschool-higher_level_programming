47916a5fdeda3fed349604f7bae37a1c9be2d8c3
glovek08 <gbrielbbarn@gmail.com>
2025-06-08 16:07:32 -0300

    fix: more error handling

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -8,18 +8,19 @@ from os import path
 
 
 def serialize_and_save_to_file(data, filename):
-    if (isinstance(filename, str)):
+    if isinstance(filename, str):
         try:
             with open(filename, mode="wb") as a_file:
                 pickle.dump(data, a_file)
         except (AttributeError, TypeError, pickle.PicklingError) as error:
             raise TypeError(f"Object cannot be serialized: {error}") from error
+        except (OSError, FileNotFoundError) as error:
+            raise OSError(f"File operation failed: {error}") from error
         except Exception:
             raise Exception
 
-
 def load_and_deserialize(filename):
-    if (isinstance(filename, str)):
+    if isinstance(filename, str):
         try:
             with open(filename, mode="rb") as a_file:
                 return pickle.load(a_file)
