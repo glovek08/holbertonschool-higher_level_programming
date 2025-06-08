8fdc51b268dcb872f33e696b1e5f26def6b72d8c
glovek08 <gbrielbbarn@gmail.com>
2025-06-08 15:39:06 -0300

    fix: raise exception instead of printing

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -12,14 +12,14 @@ def serialize_and_save_to_file(data, filename):
         try:
             with open(filename, mode="wb") as a_file:
                 pickle.dump(data, a_file)
-        except Exception as error:
-            print(error)
+        except Exception:
+            raise Exception
 
 
 def load_and_deserialize(filename):
-    if (isinstance(filename, str)) and path.exists(filename):
+    if (isinstance(filename, str)):
         try:
             with open(filename, mode="rb") as a_file:
                 return pickle.load(a_file)
-        except Exception as error:
-            print(error)
+        except Exception:
+            raise Exception
