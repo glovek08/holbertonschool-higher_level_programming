ee92cd671f2be39c5ab64b7749bf6c574f4ecd51
glovek08 <gbrielbbarn@gmail.com>
2025-06-08 22:10:39 -0300

    feat: deserialize XML file

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -23,12 +23,33 @@ def serialize_to_xml(dictionary, filename):
         try:
             ET.indent(tree, space="  ", level=0)
             tree.write(filename, encoding="utf-8")
-        except Exception as e:
-            print(f"Error writing XML to file: {e}")
+        except Exception as error:
+            print(f"Error writing XML to file: {error}")
     else:
         print("Invalid input: 'dictionary' must be a dict\
             and 'filename' must be a string.")
 
 
-def deserialize_from_xml(filename):
-    pass
+def deserialize_from_xml(filename: str) -> dict | None:
+    if not isinstance(filename, str):
+        print(f"{__name__}")
+        return None
+    if not path.exists(filename):
+        print(f"[{__name__}] CRITICAL ERROR: '{filename}")
+        return None
+    obj_reconstructed_associative_array = collections.OrderedDict()
+    try:
+        obj_xml_tree_representation = ET.parse(filename)
+        obj_xml_root_element = obj_xml_tree_representation.getroot()
+        for obj_child_element in obj_xml_root_element:
+            str_element_tag = obj_child_element.tag
+            str_element_text_content = obj_child_element.text
+            obj_reconstructed_associative_array[str_element_tag] = str_element_text_content
+        return dict(obj_reconstructed_associative_array)
+    except ET.ParseError as obj_parse_exception:
+        print(f"[{__name__}] XML fucked up: {filename} {obj_parse_exception}")
+        return None
+    except Exception as obj_generic_exception:
+        print(
+            f"[{__name__}] Something else got fucked up: {filename} {obj_generic_exception}")
+        return None
