bb2a0299d377b1531be6dce9c46ece4ba07cf536
glovek08 <gbrielbbarn@gmail.com>
2025-06-10 18:10:36 -0300

    feat: send JSON data when requesting '/data' endpoint

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -3,6 +3,8 @@
 Hosts a basic HTTP server, defines POST and GET requests
 """
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import requests
+import json
 
 
 HOST = "192.168.1.90"
@@ -15,6 +17,19 @@ class NeuralHTTP(BaseHTTPRequestHandler):
         self.end_headers()
 
         self.wfile.write(bytes("Hello, this is a simple API!", "utf-8"))
+        # accessing /data endpoint
+        if self.path == '/data':
+            try:
+                json_output = json.dumps({"name": "John", "age": 30, "city": "New York"})
+                self.send_response(200)
+                self.send_header("Content-type", "application/json")
+                self.end_headers()
+                self.wfile.write(json_output.encode('utf-8'))
+            except Exception as error:
+                print(f"Error in JSON stuff: {error}")
+                self.send_error(500, f"Server error: {error}")
+        else:
+            self.send_error(404, "Not Found")
 
 my_server = HTTPServer((HOST, PORT), NeuralHTTP)
 print("Server ON...")
