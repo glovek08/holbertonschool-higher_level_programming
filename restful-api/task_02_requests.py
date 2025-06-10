c574dfab9518f1397dc5d3f314728b8b3653d136
glovek08 <gbrielbbarn@gmail.com>
2025-06-09 22:01:43 -0300

    feat: fetch posts and format to list of dictionaries using JSON

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -6,47 +6,52 @@ import requests
 import json
 
 
-def fetch_and_print_posts():
+# def fetch_and_print_posts():
+#     try:
+#         r = requests.get("https://jsonplaceholder.typicode.com/posts")
+#         r.raise_for_status()
+#     except HTTPError as request_error:
+#         print(request_error)
+#         exit()
+#     print(f"Status Code: {r.status_code}")
+#     if (r.status_code == 200):
+#         try:
+#             json_obj = r.json()
+#         except requests.exceptions.JSONDecodeError as json_error:
+#             print(json_error)
+#             exit()
+#         for key in json_obj:
+#             print(f"{key['title']}")
+
+# fetch_and_print_posts()
+
+def fetch_and_save_posts():
     try:
         r = requests.get("https://jsonplaceholder.typicode.com/posts")
         r.raise_for_status()
     except HTTPError as request_error:
         print(request_error)
         exit()
-    print(f"Status Code: {r.status_code}")
     if (r.status_code == 200):
         try:
             json_obj = r.json()
         except requests.exceptions.JSONDecodeError as json_error:
             print(json_error)
             exit()
-        for key in json_obj:
-            print(f"{key['title']}")
-
-# fetch_and_print_posts()
-
-# def fetch_and_save_posts():
-#     try:
-#         r = requests.get("https://jsonplaceholder.typicode.com/posts")
-#         r = r.raise_for_status()
-#     except HTTPError as request_error:
-#         print(request_error)
-#         exit()
-#     try:
-#         if (r.status_code == 200):
-#             json_obj = r.json()
-#     except requests.exception.JSONDecodeError as json_error:
-#         print(json_error)
-#         exit()
-    # create new list of dict and 
-    """
-    my_list = [
-        {
-            'id': (the post's id),
-            'title': (the post's title),
-            'body': (the post's body)
-        },
-    ]
-    """
+        posts_list = [{'id': key['id'], 'title': key['title'],
+                       'body': key['body']} for key in json_obj]
+        # print(posts_list)
+        formatted_json = json.dumps(posts_list, indent=4)
+        print(formatted_json)
 
 
+fetch_and_save_posts()
+
+# create new list of dict and
+# my_list = [
+#     {
+#         'id': (the post's id),
+#         'title': (the post's title),
+#         'body': (the post's body)
+#     },
+# ]
