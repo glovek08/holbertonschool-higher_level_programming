d111a1e6ecd7168112f98e123315c5071f8151f6
glovek08 <gbrielbbarn@gmail.com>
2025-06-11 13:33:48 -0300

    feat: remaining responses, 404, 500

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -18,6 +18,24 @@ class NeuralHTTP(BaseHTTPRequestHandler):
             self.send_header("Content-type", "text/html")
             self.end_headers()
             self.wfile.write(bytes("Hello, this is a simple API!", "utf-8"))
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+        #throwing json info on info endpoint
+        elif self.path == "/info":
+            try:
+                json_output = json.dumps({"version": "1.0", 
+                    "description": "A simple API built with http.server"})
+                self.send_response(200)
+                self.send_header("Content-type", "application/json")
+                self.end_headers()
+                self.wfile.write(json_output.encode("utf-8"))
+            except Exception as error:
+                print(error)
+                self.send_error(500, f"Server error: {error}")
+                HTTPError
         # accessing /data endpoint
         elif self.path == '/data':
             try:
@@ -30,7 +48,11 @@ class NeuralHTTP(BaseHTTPRequestHandler):
             except Exception as error:
                 print(f"Error in JSON stuff: {error}")
                 self.send_error(500, f"Server error: {error}")
-
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("Endpoint not found", "utf-8"))
 
 my_server = HTTPServer((HOST, PORT), NeuralHTTP)
 # print("Server ON...")
