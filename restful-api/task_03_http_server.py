74776a828bc25beb2a179b56bba7d1304f1155e3
glovek08 <gbrielbbarn@gmail.com>
2025-06-11 10:26:07 -0300

    chore: remove server status comments

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -10,29 +10,30 @@ import json
 HOST = "localhost"
 PORT = 8000
 
+
 class NeuralHTTP(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-
-        self.wfile.write(bytes("Hello, this is a simple API!", "utf-8"))
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("Hello, this is a simple API!", "utf-8"))
         # accessing /data endpoint
-        if self.path == '/data':
+        elif self.path == '/data':
             try:
-                json_output = json.dumps({"name": "John", "age": 30, "city": "New York"})
+                json_output = json.dumps(
+                    {"name": "John", "age": 30, "city": "New York"})
                 self.send_response(200)
                 self.send_header("Content-type", "application/json")
                 self.end_headers()
-                self.wfile.write(json_output.encode('utf-8'))
+                self.wfile.write(json_output.encode("utf-8"))
             except Exception as error:
                 print(f"Error in JSON stuff: {error}")
                 self.send_error(500, f"Server error: {error}")
-        else:
-            self.send_error(404, "Not Found")
+
 
 my_server = HTTPServer((HOST, PORT), NeuralHTTP)
-print("Server ON...")
+# print("Server ON...")
 my_server.serve_forever()
 my_server.server_close()
-print("Server CLOSED.")
+# print("Server CLOSED.")
