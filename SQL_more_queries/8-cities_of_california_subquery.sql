17bb63856f7d52dfe8ddbd9bbee48623c4dcc8c2
glovek08 <gbrielbbarn@gmail.com>
2025-06-30 20:16:13 -0300

    fix: only returns where name is californication

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,6 @@
 -- @block
-SELECT cities.id, cities.state_id, cities.name
-FROM cities, states
-WHERE cities.state_id = states.id;
+SELECT id, name
+FROM cities
+WHERE state_id = (
+    SELECT id FROM states WHERE name = 'California'
+);
