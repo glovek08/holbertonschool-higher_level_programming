8b6fc070a1c23f0d493295896f0f8e36d8ca3eab
glovek08 <gbrielbbarn@gmail.com>
2025-06-30 18:32:38 -0300

    feat: grant all privileges to created user

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- @block
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD on *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
