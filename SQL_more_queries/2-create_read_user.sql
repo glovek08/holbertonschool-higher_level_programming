705b8a42d64db76c3efad9e80bee5017f8a0ab96
glovek08 <gbrielbbarn@gmail.com>
2025-06-30 18:52:59 -0300

    fix: missing database target

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- @block
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS `user_0d_2`@`localhost` IDENTIFIED WITH mysql_native_password BY 'user_0d_2_pwd';
-GRANT SELECT TO `user_0d_2`@`localhost`;
+GRANT SELECT ON hbtn_0d_2.* TO `user_0d_2`@`localhost`;
