f13abf9f92a98e7222c4887768e7a55170852bef
glovek08 <gbrielbbarn@gmail.com>
2025-07-01 19:48:13 -0300

    feat: list NULL items

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,12 +1,7 @@
 -- @block
-
-SELECT
-	tv_genres.name AS 'genre',
-	COUNT(tv_show_genres.show_id) AS 'number_of_shows'
-FROM 
-	tv_genres,
-	tv_show_genres
-WHERE
-	tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
-ORDER BY COUNT(tv_show_genres.show_id) DESC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
