8a2bddac5787e3d094b9c8224351813e28a72e68
glovek08 <gbrielbbarn@gmail.com>
2025-07-01 19:12:38 -0300

    feat: count number of shows linked to genre

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,12 +1,11 @@
 -- @block
 SELECT
-	tv_shows.title, tv_show_genres.genre_id
-FROM
-	tv_shows
-LEFT JOIN
+	tv_genres.name AS '<TV Show genre>',
+	COUNT(tv_show_genres.show_id) AS '<Number of shows linked to this genre>'
+FROM 
+	tv_genres,
 	tv_show_genres
-ON
-	tv_shows.id = tv_show_genres.show_id
-ORDER BY
-	tv_shows.title ASC,
-	tv_show_genres.genre_id ASC;
+WHERE
+	tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY COUNT(tv_show_genres.show_id) DESC;
