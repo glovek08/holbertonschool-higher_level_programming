677685274a4c8d149e42185284533f14140470af
glovek08 <gbrielbbarn@gmail.com>
2025-07-01 19:18:35 -0300

    fix: solution in correct file

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,12 +1,12 @@
 -- @block
 
+
 SELECT
-	tv_genres.name AS 'genre',
-	COUNT(tv_show_genres.show_id) AS 'number_of_shows'
-FROM 
-	tv_genres,
-	tv_show_genres
-WHERE
-	tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
-ORDER BY COUNT(tv_show_genres.show_id) DESC;
+    tv_shows.title, tv_show_genres.genre_id
+FROM
+    tv_shows
+LEFT JOIN
+    tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY
+    tv_shows.title ASC,
+    tv_show_genres.genre_id ASC;
