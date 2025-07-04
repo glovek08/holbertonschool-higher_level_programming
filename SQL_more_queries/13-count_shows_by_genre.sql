425d93f53fb33e9817b10eaafed887a307bb59e2
glovek08 <gbrielbbarn@gmail.com>
2025-07-04 12:56:30 -0300

    feat: list all genres linked to show

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,12 +1,11 @@
 -- @block
 SELECT
-	tv_genres.name AS genre,
-	COUNT(tv_show_genres.show_id) AS number_of_shows
+	tv_genres.name
 FROM
-	tv_genres
-INNER JOIN
-	tv_show_genres
-ON
+	tv_genres, tv_show_genres, tv_shows
+WHERE
 	tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.id
-ORDER BY number_of_shows DESC;
+    AND tv_shows.id = tv_show_genres.show_id
+    AND tv_shows.title = 'Dexter'
+ORDER BY
+	tv_genres.name;
