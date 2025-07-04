c36441a1153e0439869090171e2fae66e5cf7263
glovek08 <gbrielbbarn@gmail.com>
2025-07-04 12:57:09 -0300

    fix: solution in wrong script

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,11 +1,12 @@
 -- @block
 SELECT
-	tv_genres.name
+	tv_genres.name AS genre,
+	COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM
-	tv_genres, tv_show_genres, tv_shows
-WHERE
+	tv_genres
+INNER
+	tv_show_genres
+ON
 	tv_genres.id = tv_show_genres.genre_id
-    AND tv_shows.id = tv_show_genres.show_id
-    AND tv_shows.title = 'Dexter'
-ORDER BY
-	tv_genres.name;
+GROUP BY tv_genres.id
+ORDER BY number_of_shows DESC;
