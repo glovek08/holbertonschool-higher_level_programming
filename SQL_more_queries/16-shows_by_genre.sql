1acc7fbbf1a25e04f6bea48ce78640faa3f48b21
glovek08 <gbrielbbarn@gmail.com>
2025-07-04 16:10:04 -0300

    refactor: use LEFT JOIN instead to include NULL values from tv_genres

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,11 +1,15 @@
 -- @block
 SELECT
-	tv_shows.title, tv_genres.name
+	shows.title, tv_genres.name
 FROM
-	tv_shows, tv_show_genres, tv_genres
-WHERE
-	tv_shows.id = tv_show_genres.show_id
-	AND	tv_show_genres.genre_id = tv_genres.id
+	tv_shows shows
+LEFT JOIN
+	tv_show_genres
+		ON shows.id = tv_show_genres.show_id
+LEFT JOIN
+	tv_genres
+		ON tv_show_genres.genre_id = tv_genres.id
+	
 ORDER BY 
-	tv_shows.title ASC,
+	shows.title ASC,
     tv_genres.name ASC;
