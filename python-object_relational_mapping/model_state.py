c330eddfe8d05bd76f93182eee5c7b24ce38635e
glovek08 <gbrielbbarn@gmail.com>
2025-07-06 18:31:54 -0300

    chore: pycodestyle

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 This module defines the State class for SQLAlchemy ORM.
-Contains the class definition of a State and an instance Base = declarative_base()
+Contains the class definition of a State
+and an instance Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
