d8452cdec85e8b1cd13f5535b7dd6f9f2d2d5d05
glovek08 <gbrielbbarn@gmail.com>
2025-07-06 13:46:47 -0300

    doc: doctstring ffs

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3
+"""
+This script connects to a MySQL database using SQLAlchemy and retrieves all rows from the 'states' table,
+ordered by the 'id' column. The database connection parameters (username, password, database name) are
+provided as command-line arguments.
+
+Usage:
+    ./script.py <username> <password> <database>
+
+Arguments:
+    username    MySQL database username
+    password    MySQL database password
+    database    Name of the MySQL database to connect to
+
+Functionality:
+    - Establishes a connection to a MySQL database using SQLAlchemy.
+    - Reflects the 'states' table from the database schema.
+    - Executes a SELECT query to fetch all rows from the 'states' table, ordered by 'id'.
+    - Prints each row as a tuple.
+
+Raises:
+    ValueError: If the number of command-line arguments is incorrect.
+"""
 
 from sys import argv
 import MySQLdb
