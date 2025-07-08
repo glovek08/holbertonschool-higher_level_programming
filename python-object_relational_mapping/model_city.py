24c20b833f186d38678931bce2f2aa30fb024162
glovek08 <gbrielbbarn@gmail.com>
2025-07-08 13:19:08 -0300

    chore: pycodestyle

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -8,6 +8,7 @@ from model_state import Base
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import ForeignKey, String
 
+
 class City(Base):
     """
     City class that inherits from Base and links to the MySQL table cities.
@@ -15,13 +16,20 @@ class City(Base):
     Attributes:
         id: Column of auto-generated, unique integer, primary key, not null
         name: Column of string with maximum 128 characters, not null
-        state_id: Foreign key, the id of the state to which the city belongs to.
+        state_id: Foreign key, the id of the state to
+        which the city belongs to.
     """
     __tablename__ = "cities"
 
-    id: Mapped[int] = mapped_column(primary_key=True, nullable=False, autoincrement=True)
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        nullable=False,
+        autoincrement=True)
     name: Mapped[str] = mapped_column(String(128), nullable=False)
-    state_id: Mapped[int] = mapped_column(ForeignKey("states.id"), nullable=False)
+    state_id: Mapped[int] = mapped_column(
+        ForeignKey("states.id"),
+        nullable=False)
 
     def __repr__(self) -> str:
-        return f"<City(id={self.id}, name={self.name!r}, state_id={self.state_id})>"
+        return f"<City(id={self.id},\
+            name={self.name!r}, state_id={self.state_id})>"
