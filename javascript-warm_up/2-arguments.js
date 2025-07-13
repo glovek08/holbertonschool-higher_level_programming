9c855b35c1357d545706f02f0ad61af0a467ffe3
glovek08 <gbrielbbarn@gmail.com>
2025-07-13 15:51:47 -0300

    fix: exit program if no argument

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,6 +2,7 @@
 const args = process.argv;
 if (args.length === 2) {
   console.log('No argument');
+  process.exit(1);
 }
 const msg = args.length === 3 ? 'Argument found' : 'Arguments found';
 console.log(msg);
