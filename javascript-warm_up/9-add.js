6a144fa95cfe9e8b0a0db4520f608318c7c5e3fb
glovek08 <gbrielbbarn@gmail.com>
2025-07-13 18:26:20 -0300

    fix: must use console log

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,11 +2,11 @@
 const a = process.argv[2];
 const b = process.argv[3];
 if (isNaN(a) || isNaN(b)) {
-  process.stdout.write('NaN\n');
+  console.log('NaN\n');
   process.exit(1);
 } else {
   add(a, b);
 }
 function add (a, b) {
-  process.stdout.write(a + b + '\n');
+  console.log(a + b + '\n');
 }
