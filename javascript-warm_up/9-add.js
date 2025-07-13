16ab18404e3d0dfde5683c4eb83fb02e023bfe51
glovek08 <gbrielbbarn@gmail.com>
2025-07-13 18:28:51 -0300

    feat: extra newline

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,11 +2,11 @@
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
 if (isNaN(a) || isNaN(b)) {
-  console.log('NaN\n');
+  console.log('NaN');
   process.exit(1);
 } else {
   add(a, b);
 }
 function add (a, b) {
-  console.log(a + b + '\n');
+  console.log(a + b);
 }
