9642e53bc2f3446f40c7b63baa324c907e070c15
glovek08 <gbrielbbarn@gmail.com>
2025-07-21 22:28:21 -0300

    fix: trying appending to magic_string's property

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def magic_string(acc=[]):
-    acc.append("BestSchool")
-    return ", ".join(acc) + "$"
+def magic_string():
+    magic_string.my_str = getattr(magic_string, "my_str", [])
+    magic_string.my_str.append("BestSchool")
+    return ", ".join(magic_string.my_str) + "$"
