c2e171cbeebb0cb6c3477836b19bd0f85a17310a
glovek08 <gbrielbbarn@gmail.com>
2025-07-21 22:34:48 -0300

    refactor: trying using a counter again

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.my_str = getattr(magic_string, "my_str", [])
-    magic_string.my_str.append("BestSchool")
-    return ", ".join(magic_string.my_str) + "$"
+    magic_string.n = getattr(magic_string, "n", 0) + 1
+    return ", ".join(["BestSchool"] * magic_string.n) + "$"
